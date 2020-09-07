3d0f6362c2937b2410e2f7c2f6148c16f0a0324c
Jacob Levitin <jlevitin92@gmail.com>
2020-09-07 23:03:10 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,6 +1,4 @@
-def display_board(board)
 
-end
 
 def valid_move?(board, index)
   if position_taken?(board, index) == false && index.between?(0, 8)
@@ -22,6 +20,24 @@ def position_taken?(board, index)
   end
 end
 
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
 
-def move(board, index, token = "X")
+# code your input_to_index and move method here!
+
+def input_to_index(number)
+  number = number.to_i - 1
+end
+
+def move(board, index, player = "X")
+  def update_array_at_with(board, index, player)
+    board[index] = player
+  end
+  update_array_at_with(board, index, player)
+
 end
